d2e48259e2a6973c21492abe8359a7f90f5a832a
Attila Dobi <hello@adobi.hu>
2012-01-12 16:24:13 +0100

    ui stuff

--- application/views/console/index.php
+++ application/views/console/index.php
@@ -34,7 +34,7 @@
         </fieldset>    
     </div>
     <fieldset class="form-actions">
-        <button class="btn primary">Run <i class="arrow-right"></i></button> &nbsp; <a class="btn" href="<?php echo base_url() ?>/<?php echo $this->uri->segment(1) ?>">Cancel</a>
+        <button class="btn primary"> <i class="time"></i>Run query</button> &nbsp; <a class="btn" href="<?php echo base_url() ?>/<?php echo $this->uri->segment(1) ?>">Cancel</a>
     </fieldset>      
 <?php echo form_close() ?>
 
